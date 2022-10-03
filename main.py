8a2246c6e5dc189e95f05aa4004de58ad96d0142
Meet Kothari <72781025+MeetKothari@users.noreply.github.com>
2022-10-03 17:08:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ class Vector:
 
     def __repr__(self):
         return f"The vector is <{self.xcoord},{self.ycoord}>"
+    
+    def __mul__(self, x):
+        if type(x) is not int:
+            raise Exception("The built-in multiplier only allows scalar/vector multiplication. If you're looking for dotproduct or crossproduct, please refer to the 'funcs' class.")
+        self.xcoord = self.xcoord * x
+        self.ycoord = self.ycoord * x
+        return f"<{self.xcoord}, {self.ycoord}>"
 
 
 class ThDVector(Vector):
@@ -16,6 +23,11 @@ class ThDVector(Vector):
 
     def __repr__(self):
         return f"The vector is <{self.xcoord},{self.ycoord},{self.zcoord}>"
+    
+    def __mul__(self, x):
+        super().__mul__(x)
+        self.zcoord = self.zcoord * x
+        return f"<{self.xcoord}, {self.ycoord}, {self.zcoord}>"
 
 
 class funcs:
@@ -35,7 +47,7 @@ class funcs:
 
 if __name__ == '__main__':
     v1 = Vector(1, 2)
-    print(v1)
+    print(v1*3)
     
     v2 = ThDVector(1,2,3)
-    print(v2)
+    print(v2*3)
